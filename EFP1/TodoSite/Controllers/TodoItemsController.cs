93cad2b7983f509886d8033d7a5d3afac3a8b9cd
Daniel Bigelow <Interfer0@gmail.com>
2017-01-24 10:00:03 -0700

    Initial Commit of working site

---
+++ EFP1/TodoSite/Controllers/TodoItemsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EF;
+
+namespace TodoSite.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        private Project1TodoEntities db = new Project1TodoEntities();
+
+        // GET: TodoItems
+        public ActionResult Index()
+        {
+            var todoItems = db.TodoItems.Include(t => t.TodoList);
+            return View(todoItems.ToList());
+        }
+
+        // GET: TodoItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.todolistid = new SelectList(db.TodoLists, "todolistid", "title");
+            return View();
+        }
+
+        // POST: TodoItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "todoitemid,todolistid,title,description,complete")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TodoItems.Add(todoItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.todolistid = new SelectList(db.TodoLists, "todolistid", "title", todoItem.todolistid);
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.todolistid = new SelectList(db.TodoLists, "todolistid", "title", todoItem.todolistid);
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "todoitemid,todolistid,title,description,complete")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todoItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.todolistid = new SelectList(db.TodoLists, "todolistid", "title", todoItem.todolistid);
+            return View(todoItem);
+        }
+
+        // GET: TodoItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: TodoItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TodoItem todoItem = db.TodoItems.Find(id);
+            db.TodoItems.Remove(todoItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
